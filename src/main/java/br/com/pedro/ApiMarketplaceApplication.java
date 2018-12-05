d3d6a677d1d732108887ae7c71d90ee3ec2a1095
Pedro <pedroteixeirabisognin@gmail.com>
2018-12-04 23:19:31 -0200

    Adicionado arquivos do projeto

---
+++ src/main/java/br/com/pedro/ApiMarketplaceApplication.java
@@ -0,0 +1,24 @@
+/*
+ * Classname             ApiMarketplaceApplication
+ * 
+ * Version information   1.0
+ *
+ * Date                  04/12/2018
+ * 
+ * author                Pedro Teixeira Bisognin
+ * 
+ * Copyright notice      Essa aplicação foi criada para fins de estudo. 
+ */
+
+package br.com.pedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMarketplaceApplication.class, args);
+	}
+}
