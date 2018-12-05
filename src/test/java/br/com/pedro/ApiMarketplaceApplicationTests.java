c073c1e854a9a354e42793f91fb9d1b20db42fb9
Pedro <pedroteixeirabisognin@gmail.com>
2018-12-05 00:57:33 -0200

    Brincando com testes unitários

--- src/test/java/br/com/pedro/ApiMarketplaceApplicationTests.java
+++ src/test/java/br/com/pedro/ApiMarketplaceApplicationTests.java
@@ -1,9 +1,13 @@
 package br.com.pedro;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.pedro.factories.ProdutoFactory;
+import br.com.pedro.models.ProdutoAbstract;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +17,11 @@ public class ApiMarketplaceApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void produtoNull() {
+		
+		ProdutoAbstract produto = ProdutoFactory.geraProduto(null);
+		
+		Assert.assertEquals("Valor Incorreto",0.0, produto.getValor(),0.1 );
+	}
 }
